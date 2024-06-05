24e4d2d27190a0611ebe0520351f57c4ec3506fd
VeraVorobiova <weraworobiowa@mail.ru>
2024-06-05 21:17:43 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        bool insert = false;
+        private readonly MDIParent1 MainForm;
+
+        public Form1(MDIParent1 form)
+        {
+            InitializeComponent();
+            MainForm = form;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = MainForm.представление_Торговые_МаркиBindingSource;
+            bindingNavigator1.BindingSource = MainForm.представление_Торговые_МаркиBindingSource;
+            textBox1.DataBindings.Add("Text", MainForm.представление_Торговые_МаркиBindingSource, "Наименование", true, DataSourceUpdateMode.OnPropertyChanged);
+            // Выравние колонок по содержимому
+            dataGridView1.AutoResizeColumns();
+            // Скрытие поля айдишника
+            dataGridView1.Columns[0].Visible = false;
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                insert = true;
+                MainForm.представление_Торговые_МаркиBindingSource.AddNew();
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Selected = true;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (insert)
+                {
+                    if (textBox1.Text != "")
+                    {
+                        //добавление товара
+                        MainForm.queriesTableAdapter2.Марки_добавить(textBox1.Text);
+                        insert = false;
+                    }
+                }
+                else
+                {
+                    //обновление данных
+                    if (textBox1.Text != "")
+                    {
+                        //добавление товара
+                        int id;
+                        DataRowView dt;
+
+                        dt = (DataRowView)MainForm.представление_Торговые_МаркиBindingSource.Current;
+                        id = (int)dt["Код_марки"];
+                        MainForm.queriesTableAdapter2.Марки_изменить(id, textBox1.Text);
+                    }
+                }
+                MainForm.представление_Торговые_МаркиTableAdapter.Fill(MainForm.examBDDataSet.Представление_Торговые_Марки);
+                dataGridView1.AutoResizeColumns();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Ошибка сохранения данных");
+            }
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            string name = "";
+            //try
+            //{
+                int id;
+                DataRowView dt;
+
+                dt = (DataRowView)MainForm.представление_Торговые_МаркиBindingSource.Current;
+                id = (int)dt["Код_марки"];
+                name = (string)dt["Наименование"];
+
+                MainForm.queriesTableAdapter2.Марки_удалить(id);
+                MainForm.представление_Торговые_МаркиTableAdapter.Fill(MainForm.examBDDataSet.Представление_Торговые_Марки);
+           // }
+            //catch (Exception)
+            //{
+
+               // MessageBox.Show(String.Format(" Ошибка удаления"));
+            //}
+        }
+    }
+}
---
+++ MDIParent1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MDIParent1 : Form
+    {
+        private int childFormNumber = 0;
+
+        public MDIParent1()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowNewForm(object sender, EventArgs e)
+        {
+            Form childForm = new Form1(this);
+            childForm.MdiParent = this;
+            childForm.Show();
+        }
+
+        private void OpenFile(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                string FileName = openFileDialog.FileName;
+            }
+        }
+
+        private void SaveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                string FileName = saveFileDialog.FileName;
+            }
+        }
+
+        private void ExitToolsStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void PasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void ToolBarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void StatusBarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void TileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void TileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void ArrangeIconsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.ArrangeIcons);
+        }
+
+        private void CloseAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Form childForm in MdiChildren)
+            {
+                childForm.Close();
+            }
+        }
+
+        private void MDIParent1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "examBDDataSet.Второй_Отчет". При необходимости она может быть перемещена или удалена.
+            this.второй_ОтчетTableAdapter.Fill(this.examBDDataSet.Второй_Отчет);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "examBDDataSet.Первый_Отчет". При необходимости она может быть перемещена или удалена.
+            this.первый_ОтчетTableAdapter.Fill(this.examBDDataSet.Первый_Отчет);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "examBDDataSet.Представление_Акты". При необходимости она может быть перемещена или удалена.
+            this.представление_АктыTableAdapter.Fill(this.examBDDataSet.Представление_Акты);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "examBDDataSet.Представление_План". При необходимости она может быть перемещена или удалена.
+            this.представление_ПланTableAdapter.Fill(this.examBDDataSet.Представление_План);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "examBDDataSet.Представление_Торговые_Марки". При необходимости она может быть перемещена или удалена.
+            this.представление_Торговые_МаркиTableAdapter.Fill(this.examBDDataSet.Представление_Торговые_Марки);
+
+
+        }
+    }
+}
